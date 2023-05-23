b3f9943fe3a1c3a9b40596d12c44f7b67aaa66d3
Oluwaseun770 <98650324+Ogunrinde-Daniel@users.noreply.github.com>
2023-05-23 14:23:40 -0300

    I updated the json file
    The year in the json file should have just 2 numbers not 3 (2020.1 not 2020.1.18)

---
+++ UnityPackage/Runtime/Physics/ExampleScript3.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameDevDaniel.Physics
+{
+    public class ExampleScript3 : MonoBehaviour
+    {
+        public void SayHello()
+        {
+            Debug.Log("Hello3");
+        }
+
+    }
+}
