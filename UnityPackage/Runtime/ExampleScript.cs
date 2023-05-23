3e4c6fbeb047f57e18aea1cab6b1b55073eb0edd
Oluwaseun770 <98650324+Ogunrinde-Daniel@users.noreply.github.com>
2023-05-23 14:04:44 -0300

    Initial Commit
    This initial commit is a test, to see if the package works with GitHub

---
+++ UnityPackage/Runtime/ExampleScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameDevDaniel.Utils
+{
+    public class ExampleScript : MonoBehaviour
+    {
+        public void SayHello()
+        {
+            Debug.Log("Hello1");
+        }
+
+    }
+}
---
+++ UnityPackage/Runtime/ExampleScript2.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace GameDevDaniel.Utils
+{
+    public class ExampleScript2 : MonoBehaviour
+    {
+        public void SayHello()
+        {
+            Debug.Log("Hello2");
+        }
+
+    }
+}
